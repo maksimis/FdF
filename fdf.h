4eca3649627568a8b54e4113ff78d641228aba65
= <=>
2022-01-27 19:46:22 +0300

    make

---
+++ fdf.h
@@ -0,0 +1,10 @@
+#ifndef FDF_H
+# define FDFH
+
+#include <unistd.h>
+#include <math.h>
+#include	"minilibx-linux/mlx.h"
+#include	"minilibx-linux/mlx_int.h"
+
+#endif //FDF_H
+
---
+++ main.c
@@ -0,0 +1,28 @@
+#include "fdf.h"
+
+int main()
+{
+    int	a;
+void	*mlx;
+void	*win1;
+  printf("MinilibX Test Program\n");
+
+  printf(" => Connection ...");
+  if (!(mlx = mlx_init()))
+    {
+      printf(" !! KO !!\n");
+      exit(1);
+    }
+  printf("OK (use_xshm %d pshm_format %d)\n",((t_xvar *)mlx)->use_xshm,((t_xvar *)mlx)->pshm_format);
+
+  printf(" => Window1 %dx%d \"Title 1\" ...",300,300);
+  if (!(win1 = mlx_new_window(mlx,300,300,"Title1")))
+    {
+      printf(" !! KO !!\n");
+      exit(1);
+    }
+  printf("OK\n");
+  mlx_pixel_put(mlx, win1, 10, 10, 0xFFFFFF);
+   sleep(10);
+    return(0);
+}
